8b50bb05612edd5305307d32ed1072b0287197d9
Rian <rian.zenki@gmail.com>
2024-04-28 20:19:39 -0300

    feat: adjust button to receive variant props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,28 @@
 import { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 
-interface ButtonProps extends ComponentProps<"button"> {}
+interface ButtonProps extends ComponentProps<"button"> {
+   variant?: "solid" | "outline" | "link";
+}
 
-export const Button = ({ children, ...rest }: ButtonProps) => {
+export const Button = ({
+   children,
+   variant = "solid",
+   ...rest
+}: ButtonProps) => {
+   const variantStyles = {
+      solid: "bg-brandColor-700 text-white hover:bg-[#2672f9] shadow",
+      outline:
+         "bg-transparent border border-brandColor-700 text-brandColor-700 shadow",
+      link: "bg-transparent text-brandColor-700",
+   };
+
    return (
       <button
          {...rest}
          className={twMerge(
-            `w-[200px] h-12 px-5 py-4 rounded bg-brandColor-700 text-white transition hover:bg-[#2672f9] flex items-center justify-center gap-2`,
+            `w-[200px] h-12 px-5 py-4 rounded transition flex items-center justify-center gap-2`,
+            variantStyles[variant],
             rest.className
          )}
       >
